1c038819500ddccb07a9b51e5eb475cc7431c78d
Emilia Galano <80059240+emilyaahhg@users.noreply.github.com>
2022-08-12 15:32:20 +0800

    Update main.py
    change line 13

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import cv2
-
+import cv2 #pip install opencv-python==4.5.5.64
 thres = 0.5 #Threshold to detect objects;)
 
 # img = cv2.imread('dog.jpg')
@@ -11,7 +10,7 @@ cap.set(4,480)
 classNames = []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+     classNames = [line.rstrip() for line in f]
 
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'frozen_inference_graph.pb'
